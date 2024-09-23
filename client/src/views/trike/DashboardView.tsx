5ef590068ce5109c1cbf7612fbd2e18da01c2327
Yixu Wang <wsonny3771@gmail.com>
2024-09-23 17:38:46 +1000

    Prevent component overlaps and add adaptive grid layout based on screen size

--- client/src/views/trike/DashboardView.tsx
+++ client/src/views/trike/DashboardView.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import TrikeLocationMap from 'components/trike/dashboard/TrikeLocationMap';
 
-import { Col, Row } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
 
 import styles from './DashboardView.module.css';
 import { TrikeSpeedDistanceChart } from '../../components/trike/charts/TrikeSpeedDistanceChart';
@@ -14,17 +14,33 @@ import { TrikeSpeedDistanceChart } from '../../components/trike/charts/TrikeSpee
  */
 export default function DashboardView(): JSX.Element {
   return (
-    <Row className={styles.contentContainer}>
-      <Col lg={6} className={styles.content}>
-        <div className={styles.bigGraph}>
-          <TrikeSpeedDistanceChart />
-        </div>
-      </Col>
-      <Col lg={6} className={styles.content}>
-        <div className={styles.bigGraph}>
-          <TrikeLocationMap />
-        </div>
-      </Col>
-    </Row>
+    <Container fluid>
+      <Row className={styles.contentContainer}>
+        <Col
+          xs={{ span: 12, order: 1 }}
+          lg={{ span: 6, order: 1 }}
+          className={styles.statContainer}
+        >
+          Statistics Here
+        </Col>
+        <Col xs={{ span: 12, order: 3 }} lg={{ span: 6, order: 2 }}>
+          <div className={styles.graph}>
+            <TrikeSpeedDistanceChart />
+          </div>
+        </Col>
+        <Col
+          xs={{ span: 12, order: 2 }}
+          lg={{ span: 6, order: 3 }}
+          className={styles.statContainer}
+        >
+          Statistics Here
+        </Col>
+        <Col xs={{ span: 12, order: 4 }} lg={{ span: 6, order: 4 }}>
+          <div className={styles.bigGraph}>
+            <TrikeLocationMap />
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
