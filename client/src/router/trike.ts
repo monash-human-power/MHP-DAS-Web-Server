58152481b49df8a38e32ae48cbe48f5a6638bc32
eneribackwards <140178583+eneribackwards@users.noreply.github.com>
2024-09-23 13:22:08 +1000

    Finished the router to better reflect the mockup; added the common views

--- client/src/router/trike.ts
+++ client/src/router/trike.ts
@@ -2,6 +2,9 @@ import React from 'react';
 import { RouteInfo } from 'types/route';
 import DashboardView from 'views/trike/DashboardView';
 import ChartView from 'views/trike/ChartView';
+import BoostView from 'views/common/BoostView';
+import LogsView from 'views/common/LogsView';
+import CameraSystemView from 'views/common/CameraSystemView';
 
 const routes: RouteInfo[] = [
   {
@@ -15,6 +18,24 @@ const routes: RouteInfo[] = [
     path: '/trike/charts',
     exact: true,
     component: (ChartView as unknown) as React.Component,
+  },
+  {
+    name: 'Boost',
+    path: '/v3/boost',
+    exact: true,
+    component: (BoostView as unknown) as React.Component,
+  },
+  {
+    name: 'Camera System',
+    path: '/v3/camera-system',
+    exact: true,
+    component: (CameraSystemView as unknown) as React.Component,
+  },
+  {
+    name: 'Logs',
+    path: '/v3/logs',
+    exact: true,
+    component: (LogsView as unknown) as React.Component,
   },
 ];
 
