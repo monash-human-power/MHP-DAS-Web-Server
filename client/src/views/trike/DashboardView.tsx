dcfa40d2af069c10eb6e6e272ad06109d7e84c69
eneribackwards <140178583+eneribackwards@users.noreply.github.com>
2024-09-23 14:22:53 +1000

    add dashboard routing

--- client/src/views/trike/DashboardView.tsx
+++ client/src/views/trike/DashboardView.tsx
@@ -5,6 +5,7 @@ import TrikeLocationMap from 'components/trike/dashboard/TrikeLocationMap';
 import { Col, Row } from 'react-bootstrap';
 
 import styles from './DashboardView.module.css';
+import { TrikeSpeedDistanceChart } from '../../components/trike/charts/TrikeSpeedDistanceChart';
 
 /**
  * Dashboard view component
@@ -14,7 +15,15 @@ import styles from './DashboardView.module.css';
 export default function DashboardView(): JSX.Element {
   return (
     <Row className={styles.contentContainer}>
-      <Col className={styles.content}>
+      <Col lg={6} className={styles.content}>
+        <div className={styles.bigGraph}>
+          <TrikeSpeedDistanceChart />
+        </div>
+        <div className={styles.bigGraph}>
+          <TrikeSpeedDistanceChart />
+        </div>
+      </Col>
+      <Col lg={6} className={styles.content}>
         <div className={styles.bigGraph}>
           <TrikeLocationMap />
         </div>
