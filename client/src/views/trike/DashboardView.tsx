fed7287e029aae89f748afdffb7adca5d2d8aa33
eneribackwards <140178583+eneribackwards@users.noreply.github.com>
2024-09-25 13:56:14 +1000

    replace with animated trike map in dashboard

--- client/src/views/trike/DashboardView.tsx
+++ client/src/views/trike/DashboardView.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import TrikeLocationMap from 'components/trike/dashboard/TrikeLocationMap';
+// import TrikeLocationMap from 'components/trike/dashboard/TrikeLocationMap';
+import AnimatedLocationMap from 'components/trike/dashboard/AnimatedLocationMap';
 
 import { Container, Col, Row } from 'react-bootstrap';
 import LiveDataRow from 'components/trike/dashboard/LiveDataRow';
@@ -39,7 +40,7 @@ export default function DashboardView(): JSX.Element {
         </Col>
         <Col xs={{ span: 12, order: 4 }} lg={{ span: 6, order: 4 }}>
           <div className={styles.bigGraph}>
-            <TrikeLocationMap />
+            <AnimatedLocationMap />
           </div>
         </Col>
       </Row>
