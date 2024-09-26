33b1b6838a02bc1672b15cbc5b7465d3643e1ed6
aarabikug <aarabikugathas@gmail.com>
2024-09-26 12:49:32 +1000

    adding elapsed time function - still need to fix

--- client/src/components/trike/dashboard/TrikeStats.tsx
+++ client/src/components/trike/dashboard/TrikeStats.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './LiveDataRow.module.css';
 import LiveData from './LiveData';
 
 export default function TrikeStatsRow(): JSX.Element {
+  // Elapsed time function
+  const [localTime, setLocalTime] = useState(new Date());
+  const [elapsedTime, setElapsedTime] = useState(0); // In seconds
+  const startTime = new Date(); // Capture the start time
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Update local time
+      const now = localTime;
+      setLocalTime(now);
+
+      // Calculate elapsed time (in seconds)
+      const elapsed = Math.floor(
+        (localTime.getTime() - startTime.getTime()) / 1000,
+      ); // in seconds
+
+      setElapsedTime(elapsed);
+    }, 1000); // Update every second
+
+    return () => clearInterval(interval); // Clear interval on component unmount
+  }, [startTime]);
+
   return (
     <div>
       {/** LAP DATA */}
@@ -31,11 +53,11 @@ export default function TrikeStatsRow(): JSX.Element {
 
       <div className={styles.currentStintContainer}>
         <div className={styles.liveDataItem}>
-          <LiveData value="to be finished" unit="km/h" desc="Elapsed Time:" />
+          <LiveData value={elapsedTime} unit="sec" desc="Elapsed Time:" />
         </div>
 
         <div className={styles.liveDataItem}>
-          <LiveData value="300" unit="km/h" desc="Avg Spd:" />
+          <LiveData value="vals" unit="km/h" desc="Avg Spd:" />
         </div>
 
         <div className={styles.liveDataItem}>
